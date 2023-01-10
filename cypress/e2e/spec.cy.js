2c8d8b0b961bb91affdba1a860bae857756eebb6
Yordi Verschure <459372@student.fontys.nl>
2023-01-10 13:46:00 +0100

    cypress test 1

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,5 @@
+describe("home page", () => {
+  it("loading passes", () => {
+    cy.visit("<http://localhost:3000>");
+  });
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+describe("home page", () => {
+  it("loading passes", () => {
+    cy.visit("<http://localhost:3000>");
+  });
+});
