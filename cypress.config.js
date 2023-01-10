8867030300a1dba2f2ffad64ad0a3d9d645b47c8
Yordi Verschure <459372@student.fontys.nl>
2023-01-10 14:19:36 +0100

    cypress easy test. FIXED

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ip9nab',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,9 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
-describe("home page", () => {
-  it("loading passes", () => {
-    cy.visit("<http://localhost:3000>");
-  });
-});
